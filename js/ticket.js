69da9d795ef5f1020cca64750ea311d4189645fe
rebakpi@gmail.com <rebakpi@gmail.com>
2024-02-19 15:35:20 +0600

    added the html file

--- js/ticket.js
+++ js/ticket.js
@@ -1,1 +1,29 @@
-alert('hello')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//document.getElementById('buyTicket').addEventListener('click', function() {
+//      var hiddenSection = document.getElementById('green-line');
+//      
+//      // Toggle the display property directly
+//      hiddenSection.style.display = (hiddenSection.style.display === 'none' || hiddenSection.style.display === '') ? 'block' : 'none';
+//    });
